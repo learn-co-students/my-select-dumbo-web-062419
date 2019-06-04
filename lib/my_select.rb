07b2829a08178d6f7002e1c8b2d8cb87ba6f90e1
Olga Rosas <olga.rosas@gmail.com>
2019-06-04 17:26:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+i = 0
+select = []
+  while i < array.length
+    if (yield(array[i]))
+      select.push(array[i])
+  end
+  i += 1
 end
+select
+end
+
+ my_select([1, 2, 3, 4]) do |num|
+    num.even?
+    end
