3c0b31cd7156a00044cd4b1fb2985648e685c2f4
emanovic7 <e_owusu@live.com>
2019-06-07 00:23:39 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-# A sample Gemfile
-source "https://rubygems.org"
-
-# gem "rails"
-
-gem 'rspec'
-gem 'pry'
+# # A sample Gemfile
+# source "https://rubygems.org"
+#
+# # gem "rails"
+#
+# gem 'rspec'
+# gem 'pry'
--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
  # your code here!
+ selected_array = []
+
+ if block_given?
+
+   i=0
+   while i <collection.length
+     current_element = collection[i]
+     if yield current_element
+       selected_array << current_element
+     end
+     i += 1
+   end
+
+ else
+   "block not given"
+ end
+ selected_array
 end
