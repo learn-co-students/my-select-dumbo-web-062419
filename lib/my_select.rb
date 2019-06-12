dd555b087fa347dcd36d9df5204ed3ddf631582b
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-12 17:51:06 -0400

    finished my_select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+	n = collection.length
+	i = 0
+	narr = []
+	ret = 0
+	while i < n
+		ret = yield collection[i]
+		# if the return value evaluates to true
+		if !!ret == true
+			narr << collection[i]
+		end
+		i += 1
+	end
+	narr
 end
