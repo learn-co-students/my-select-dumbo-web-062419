89a62a4b6fe176c7df35d654ab00a2f3be697665
Olga Rosas <olga.rosas@gmail.com>
2019-06-19 12:24:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,24 @@
-def my_select(array)
-i = 0
-select = []
-  while i < array.length
-    if (yield(array[i]))
-      select.push(array[i])
-  end
-  i += 1
-end
-select
-end
+require 'pry'
+#def my_select(array)
+#i = 0
+#select = []
+#  while i < array.length
+#    if (yield(array[i]))
+#      select.push(array[i])
+#  end
+#  i += 1
+#end
+#select
+#end
 
- my_select([1, 2, 3, 4]) do |num|
-    num.even?
-    end
+# my_select([1, 2, 3, 4]) do |num|
+#    num.even?
+#    end
+def hello(array)
+  i = 0
+  while i < array.length
+    binding.pry
+    yield(array[i])
+    i += 1
+  end
+end
